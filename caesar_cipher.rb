c8841dc44365c5e73105c677d3fe36622482dca9
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-20 13:17:39 +0100

    Add validation
    Added validation, not sure if it is actually validating yet since the code isn’t complete yet but it should validate the input.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,10 +1,14 @@
-def caesar_cipher(str, shift = 3)
+def caesar_cipher(str, shift = -3)
   # store ascii values of string in variable
   # A-Z == 65 - 90
   # a-z == 97 - 122
-  ascii_values = str.split('').map {|x| x.ord}
+  num_values = str.split('').map {|x| x.ord}
   # validate input
-  # create logic for wrapping
+  if num_values.between?(65, 90) || num_values.between?(97, 122)
+    # create logic for wrapping
+
+  end
   # translate to caesar shift
   # return caesar cipher
+  p result
 end
