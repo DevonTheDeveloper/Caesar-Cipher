f8c6e4bc824fc4b59fafec161b82f79ec8b83960
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-20 13:03:16 +0100

    Store ascii values of string

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,7 +1,10 @@
 def caesar_cipher(str, shift = 3)
-  # assign the alphabet to a variable
-  # create logic for wrapping
+  # store ascii values of string in variable
+  # A-Z == 65 - 90
+  # a-z == 97 - 122
+  ascii_values = str.split('').map {|x| x.ord}
   # validate input
+  # create logic for wrapping
   # translate to caesar shift
   # return caesar cipher
 end
