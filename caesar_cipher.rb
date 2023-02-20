2979f4a4d8a1638c955e5fce16047dc99678d27d
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-20 15:15:29 +0100

    Allow punctuation &  other characters
    Allow punctuation and other characters & add wrapping the input

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,4 +1,4 @@
-def caesar_cipher(str, shift = -3)
+def caesar_cipher(str, shift = 3)
   # store ascii values of string in variable
   # A-Z == 65 - 90
   # a-z == 97 - 122
@@ -6,14 +6,27 @@ def caesar_cipher(str, shift = -3)
   # encrypt the string
   encrypt = num_values.map do |char|
     # validate input
-    if char.between?(65, 90) || char.between?(97, 122)
+    if char.between?(65, 90)
       # wrap input & calculate
-      char + shift > 90 || char + shift > 122 ? (char + shift) - 26 : char + shift
+      if shift >= 0 
+        char + shift > 90 ? (char + shift) - 26 : char + shift
+      else
+        char + shift < 65 ? (char + shift) + 26 : char + shift
+      end
+    elsif char.between?(97, 122)
+      # wrap input & calculate
+      if shift >= 0
+        char + shift > 122 ? (char + shift) - 26 : char + shift
+      else
+        char + shift < 97 ? (char + shift) + 26 : char + shift
+      end
     else
-      return
+      # allow punctuation and other characters
+      char
     end
   end
   encrypted_str = encrypt.map! {|x| x.chr}.join
   # return encrypted string
+  p encrypted_str
   return encrypted_str
 end
