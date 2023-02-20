b16cbc177b23aee88e86261c2fd6dbb6d987851e
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-20 14:46:04 +0100

    Add validation

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,12 +3,14 @@ def caesar_cipher(str, shift = -3)
   # A-Z == 65 - 90
   # a-z == 97 - 122
   num_values = str.split('').map {|x| x.ord}
-  # validate input
-  if num_values.between?(65, 90) || num_values.between?(97, 122)
-    # create logic for wrapping
-
+  encrypt = num_values.map do |char|
+    # validate input
+    if char.between?(65, 90) || char.between?(97, 122)
+        char + shift
+    else
+      return
+    end
   end
-  # translate to caesar shift
   # return caesar cipher
-  p result
+  return encrypted_str
 end
