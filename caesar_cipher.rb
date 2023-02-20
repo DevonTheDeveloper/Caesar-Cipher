189a06306a3cec72ebdee871cf615d56933d724e
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-20 11:37:25 +0100

    Add pseudo-code

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,7 @@
-def caesar_cipher(text, shift)
-
+def caesar_cipher(str, shift = 3)
+  # assign the alphabet to a variable
+  # create logic for wrapping
+  # validate input
+  # translate to caesar shift
+  # return caesar cipher
 end
